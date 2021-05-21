b48226e20d2da464ee53ada42fd8e97fd7e7805b
zellytozelly <zellytozelly@gmail.com>
2021-05-21 16:42:37 +0900

    FEAT: add nodemon, set mysql

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
+
+
 const express = require("express"),
     app = express();
     http = require('http').createServer(app),
     port = 3001;
 
-app.use(express.static(__dirname + "/views")); //views 경로 static으로 만들기.
 
+//MySQL 설정 추가
+const mysql = require('mysql');
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: ''
+});
+
+//DB연결확인
+con.connect(function(err){
+    if(err)throw err;
+    console.log('MySQL Connected');
+})
+
+app.use(express.static(__dirname + "/views")); //views 경로 static으로 만들기.
 
 const routes = require("./routes/"); //index.js
 app.use(routes)                      //use:경로 확장성
